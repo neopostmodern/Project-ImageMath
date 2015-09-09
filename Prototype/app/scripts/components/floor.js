baca951a640b621199b6d5f48fa6854f86adee99
neo post modern <clemens@neopostmodern.com>
2015-09-09 23:21:01 +0200

    Gender-equal human representation

--- Prototype/app/scripts/components/floor.js
+++ Prototype/app/scripts/components/floor.js
@@ -1,7 +1,43 @@
 import React from 'react';
 import classNames from 'classnames';
 
+const HumanImages = [
+  "images/people/man.svg",
+  "images/people/woman.svg"
+];
+
 export default class Floor extends React.Component {
+  constructor() {
+    super()
+
+
+    let grid = new Array(10);
+    for (var i = 0; i < 10; i++) {
+      grid[i] = new Array(10);
+      for (var j = 0; j < 10; j++) {
+        grid[i][j] = null;
+      }
+    }
+
+    this.state = {
+      currentHumanImageId: 0,
+      humanImageGrid: grid
+    }
+  }
+
+  _onToggleCell(x, y) {
+    if (!this.props.grid[x][y]) { // activating, currently inactive
+      let change = {};
+      change[x] = {};
+      change[x][y] = { $set: this.state.currentHumanImageId };
+      this.setState({ humanImageGrid: React.addons.update(this.state.humanImageGrid, change) });
+
+      this.setState({currentHumanImageId: (this.state.currentHumanImageId + 1) % HumanImages.length});
+    }
+
+    this.props.onToggleCell(x, y);
+  }
+
   render () {
     let grid = [];
     let humans = [];
@@ -23,7 +59,16 @@ export default class Floor extends React.Component {
             left : (-135 + inverted_y * 15) + x * (93 - inverted_y * 3.3)
           };
 
-          humans.push(<img className='human' style={position} src="images/people/man.svg" />);
+          let humanImage; // can't ternary because active expression mustn't be evaluated before-hand
+          if (cellActive) {
+            humanImage = HumanImages[this.state.humanImageGrid[x][y]];
+          } else {
+            humanImage = HumanImages[this.state.currentHumanImageId];
+          }
+
+          humans.push(<img className='human'
+                           style={position}
+                           src={humanImage} />);
         }
 
         let classes = classNames('cell',
@@ -35,7 +80,7 @@ export default class Floor extends React.Component {
           <div className={classes}
                key={'f-' + y + '-' + x}
                onMouseEnter={this.props.onMouseEnterCell.bind(null, x, y)}
-               onClick={this.props.onToggleCell.bind(null, x, y)}>
+               onClick={this._onToggleCell.bind(this, x, y)}>
           </div>);
       }
 
